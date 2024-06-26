7a28f64c94bcd9562287f683b8f8201cbda1ee11
An8bit <ttkit35@gmail.com>
2024-06-26 18:37:29 +0700

    fix bug thuan

--- meochill/lib/widget/screens/sort/tab_controller.dart
+++ meochill/lib/widget/screens/sort/tab_controller.dart
@@ -1,144 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meochill/common/enum/category_item.dart';
 import 'package:meochill/common/enum/load_status.dart';
 import 'package:meochill/repostsitories/api.dart';
-import 'package:meochill/widget/screens/search/cubit/search_cubit.dart';
-import 'package:meochill/widget/screens/search/cubit/search_state.dart';
-import 'package:meochill/widget/screens/search/widget/search_ideal.dart';
-import 'package:meochill/widget/screens/search/widget/search_result.dart';
 import 'package:meochill/widget/screens/sort/cubit/sort_cubit.dart';
 import 'package:meochill/widget/screens/sort/cubit/sort_state.dart';
 import 'package:meochill/widget/screens/sort/widget/main_sort_screen.dart';
 
+
 class CustomTabBar extends StatelessWidget {
-  const CustomTabBar({super.key});
+  const CustomTabBar({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => SortCubit(context.read<Api>()),
-      child: BlocConsumer<SortCubit, SortState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return Main();
-        },
-      ),
+      child: Main(),
     );
   }
 }
 
-
-  DefaultTabController Main() {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                child: Text(
-                  "Hoạt Hình ",
-                  style: TextStyle(color: Colors.white),
-                ),
+DefaultTabController Main() {
+  return DefaultTabController(
+    length: 4,
+    child: Scaffold(
+      appBar: AppBar(
+        bottom: const TabBar(
+          tabs: [
+             Tab(
+              child: Text(
+                "Hoạt Hình ",
+                style: TextStyle(color: Colors.white),
               ),
-              Tab(
-                child: Text(
-                  "Hanh Dong",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Tab(
-                child: Text(
-                  "Hanh dong",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Tab(
-                child: Text(
-                  "Hài Kịch",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-           
-            BlocBuilder<SortCubit, SortState>(
-              builder: (context, state) {
-                
-                if (state.loadStatus == LoadStatus.Loading) {
-                  return const Center(child: CircularProgressIndicator(),);
-                }else if(state.loadStatus == LoadStatus.Error){
-                  
-                 return Center(child: Text("null"),);
-                }else{
-                  return MainSortScreen();
-                }
-              },
             ),
-            HanhDong(),
-            Center(child:Text("null")),
-            Center(child: Text("null")),
+             Tab(
+              child: Text(
+                "Hành Động",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+             Tab(
+              child: Text(
+                "Hài Kịch",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+             Tab(
+              child: Text(
+                "Tinh Cảm",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           ],
         ),
       ),
+      body:const TabBarView(
+        children:  [
+          TabContent(tabType: "hoathinh"),
+          TabContent(tabType: "hanhdong"),
+          TabContent(tabType: "hai"),
+          TabContent(tabType: "tinhcam"),
+        ],
+      ),
+    ),
+  );
+}
+  
+class TabContent extends StatelessWidget {
+  final String tabType;
+
+  const TabContent({Key? key, required this.tabType}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = BlocProvider.of<SortCubit>(context);
+    cubit.getTypeMovie(tabType);
+
+    return RefreshIndicator(
+      child: BlocBuilder<SortCubit, SortState>(
+        builder: (context, state) {
+          if (state.loadStatus == LoadStatus.Loading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state.loadStatus == LoadStatus.Error) {
+            return const Center(child: Text('Error'));
+          } else {
+            // Replace with your UI for each tab type
+            return const MainSortScreen();
+            }
+          }
+      ),
+      onRefresh: () async {
+        cubit.getTypeMovie(tabType);
+      },
     );
-  }
-
-class HanhDong extends StatelessWidget {
-  const HanhDong({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_){
-      BlocProvider.of<SortCubit>(context).getTypehanhdong();
-    });
-    return RefreshIndicator(child: BlocBuilder<SortCubit, SortState>(
-      builder: (context, state) {
-        if(state.loadStatus==LoadStatus.Loading){
-           return Center(child: const CircularProgressIndicator());
-        }
-        else if(state.loadStatus==LoadStatus.Error){
-          return Center(child: Text('null'));
-        }
-        else{
-          return MainSortScreen();
-        }
-      },
-
-    ), onRefresh: ()async {
-      BlocProvider.of<SortCubit>(context).getTypehanhdong();
-    });
- 
-  }
-  }
-  class HoatHinh extends StatelessWidget {
-  const HoatHinh({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_){
-      BlocProvider.of<SortCubit>(context).getTypeMovie();
-    });
-    return RefreshIndicator(child: BlocBuilder<SortCubit, SortState>(
-      builder: (context, state) {
-        if(state.loadStatus==LoadStatus.Loading){
-           return Center(child: const CircularProgressIndicator());
-        }
-        else if(state.loadStatus==LoadStatus.Error){
-          return Center(child: Text('null'));
-        }
-        else{
-          return MainSortScreen();
-        }
-      },
-
-    ), onRefresh: ()async {
-      BlocProvider.of<SortCubit>(context).getTypeMovie();
-    });
- 
   }
 }
--- meochill/lib/widget/screens/sort/widget/main_sort_screen.dart
+++ meochill/lib/widget/screens/sort/widget/main_sort_screen.dart
@@ -12,30 +12,33 @@ class MainSortScreen extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-       
-       
-        Expanded(child: BlocBuilder<SortCubit, SortState>(
-          builder: (context, state) {
-               if (state.movies.isNotEmpty) {
-                return GridView.count(
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              crossAxisCount: 3,
-              childAspectRatio: 1 / 1.4,
-              shrinkWrap: true,
-              children: List.generate(state.movies.length, (index) {
-                final movie = state.movies [index];
-                 return  MainCard(
-                  id: "id",
-                  imageUrl:movie.thumburl ?? 'default_image_url',
-                  movie: movie,
+        // Bạn có thể thêm các widget khác ở đây nếu cần thiết
+        Expanded(
+          child: BlocBuilder<SortCubit, SortState>(
+            builder: (context, state) {
+              if (state.movies.isNotEmpty) {
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 1 / 1.4,
+                  ),
+                  itemCount: state.movies.length,
+                  itemBuilder: (context, index) {
+                    final movie = state.movies[index];
+                    return MainCard(
+                      id: "id",
+                      imageUrl: movie.thumburl ?? 'default_image_url',
+                      movie: movie,
+                    );
+                  },
                 );
-              }),
-            );
-        }
-       return const Center(child: Text("No results found"));
-          },
-        ))
+              }
+              return const Center(child: Text("No results found"));
+            },
+          ),
+        ),
       ],
     );
   }
