8025648e7a98784571ab01d513bc6cb88aaee0ee
An8bit <ttkit35@gmail.com>
2024-06-26 17:26:49 +0700

    main

--- meochill/lib/widget/screens/home/cubit/home_cubit.dart
+++ meochill/lib/widget/screens/home/cubit/home_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meochill/common/enum/load_status.dart';
+
+import '../../../../models/movie2.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  HomeCubit() : super(HomeState.init());
 }
--- meochill/lib/widget/screens/home/cubit/home_state.dart
+++ meochill/lib/widget/screens/home/cubit/home_state.dart
@@ -1,10 +1,51 @@
 part of 'home_cubit.dart';
 
-sealed class HomeState extends Equatable {
-  const HomeState();
+ class HomeState {
+ final LoadStatus loadStatus;
+final List<Movie> movies;
+HomeState({
+  required this.loadStatus,
+  required this.movies,
+});
+HomeState.init({
+  this.loadStatus = LoadStatus.Init,
+  this.movies = const [],
+});
+HomeState copyWith({
+  LoadStatus? loadStatus,
+  List<Movie>? movies,
+}) {
+  return HomeState(
+    loadStatus: loadStatus ?? this.loadStatus,
+    movies: movies ?? this.movies,
+  );
 
-  @override
-  List<Object> get props => [];
 }
+@override
+bool operator ==(covariant HomeState other) {
+  if (identical(this, other)) return true;
+  return listEquals(other.movies, movies) &&
+      other.loadStatus == loadStatus;
 
-final class HomeInitial extends HomeState {}
+
+}
+@override
+int get hashCode => movies.hashCode ^ loadStatus.hashCode;
+@override
+String toString() => 'HomeState(movies: $movies, loadStatus: $loadStatus)';
+Map<String, dynamic> toMap() {
+  return <String, dynamic>{
+    'movies': movies,
+    'loadStatus': loadStatus,
+  };
+
+}
+factory HomeState.fromMap(Map<String, dynamic> map) {
+  return HomeState(
+    movies: List<Movie>.from(map['movies'] as List<Movie>),
+    loadStatus: map['loadStatus'] as LoadStatus,
+  );
+ 
+ }
+ }
+
